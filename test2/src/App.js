a05fc765ad82484546a66ac4f12911d9291210fe
AbhiShukl18 <164164042+AbhiShukl18@users.noreply.github.com>
2024-07-05 22:02:37 +0530

    update

---
+++ test2/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+
+      <Route path="/" element={<ABC/>} />
+
+
+
+      </Routes>
+   <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
